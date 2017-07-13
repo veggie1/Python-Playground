4643a5d8a1670272f586e82a5a7651a36489c5f5
Ben <ben.baacke@gmail.com>
2017-07-13 18:25:10 -0400

    Starting Django Web App
    Using a tutorial to make a Django web app.
    
    - Currently only the admin portion works, but it's something to start with

---
+++ DjangoWebApp/DjangoWebApp/DjangoWebApp/urls.py
@@ -0,0 +1,24 @@
+"""
+Definition of urls for DjangoWebApp.
+"""
+
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', DjangoWebApp.views.home, name='home'),
+    # url(r'^DjangoWebApp/', include('DjangoWebApp.DjangoWebApp.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+]
